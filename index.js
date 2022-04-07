2f75a6f258dfbc1d9536f277ceb5c5e4e0f16093
Lidianys <lidianysrodriguez96@gmail.com>
2022-04-07 09:31:53 -0400

    funcion de complilacion de array

--- index.js
+++ index.js
@@ -4,15 +4,16 @@
 
 const readline = require('readline');
 const app = require('./app.js');
-let array = [];
-
+let colors = require('colors');
+// let colors = require('colors/safe');
+let arrayObject = [];
 let interfazCapture = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 interfazCapture.question('Ingrese la ruta:', function(resp){
-    console.log(`Su ruta es: ${resp}`)
+    console.log(`Su ruta es: ${resp}`);
     // filesCarpet(resp);
     console.log('las carpetas son:', resp);
     app.verifyAcces(resp).then(() => { 
@@ -25,26 +26,42 @@ interfazCapture.question('Ingrese la ruta:', function(resp){
   }).then((resp) => {
       if (app.isFile(resp) ) {
         if (app.pathExtension(resp)) {
-            console.log('contiene un docuemnto .md');
+            console.log('contiene un docuemnto .md'.bgGreen);
+          
+
         }
+       
         else{
-            console.log('No es un .md');
+            // console.log(colors.bgRed('No es un .md'));
+            console.log('No es un archivo .md'.bgRed);
+            throw new TypeError();
         }  
       }
-    else{
+        else{
         console.log('No es un archivo');
         throw new TypeError();
 
     }
     return resp;
  }).then((resp) => {
-    //  app.readLines(resp);
-    array = app.readLines(resp);
-    
-    console.log("lectura de lineas", array );
-    return(array);
- })
+  
+    return app.readLines(resp);
 
-    });
-   
-// interfazCapture.close()
+ }).then((arrayLink) => {
+    console.log('El array1 tiene que el valor:', arrayLink);
+    const promiseArr = arrayLink.map((url) => app.verifyLinks(url).then((status) => {
+      arrayObject.push(status);
+    })
+      .catch((err) => {
+        console.log('La ruta  no existe');
+        console.log(err);
+      }));
+    return Promise.all(promiseArr);
+
+  
+}).then((promiseArr) =>{
+    console.log('el array de objetos es ', arrayObject);
+})
+interfazCapture.close();
+
+});
